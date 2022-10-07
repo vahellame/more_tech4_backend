fc5bbcd6849aa1378eb48d7b2238fbdebe118c14
Vadim R <vahellame@gmail.com>
2022-10-07 20:20:33 +0300

    add requirements.txt

--- app.py
+++ app.py
@@ -13,34 +13,6 @@ CORS(app)
 def handle_login():
     return process_login(request)
 
-# [Unit]
-#
-# Description=FinArbitr Server
-#
-# [Service]
-#
-# Type=oheshot
-# WorkingDirectory=/home/fa/finarbitr/backend
-# ExecStart=/usr/sbin/runuser -l fa -c '/home/fa/finarbitr/backend/venv/bin/gunicorn --chdir /home/fa/finarbitr/backend app:app -b 0.0.0.0:8080 --threads=1 --workers=4 --reload'
-#
-# [Install]
-#
-# WantedBy=multi-user.target
-
-# [Unit]
-#
-# Description=FinArbitr Server
-#
-# [Service]
-#
-# Type=oheshot
-#
-# ExecStart=/home/fa/finarbitr/autodeploy/venv/bin/python /home/fa/finarbitr/autodeploy/main.py
-#
-# [Install]
-#
-# WantedBy=multi-user.target
-
 
 @app.route("/test", methods=['GET', 'POST'])
 def handle_test():
