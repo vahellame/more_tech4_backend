bf5fb5d57dc0b6a5a2cf14ad9fe235e424947495
Vadim R <vahellame@gmail.com>
2022-10-08 12:23:24 +0300

    update app.py

--- src/methods/upload_photo.py
+++ src/methods/upload_photo.py
@@ -10,9 +10,10 @@ from src.config import PHOTOS_DIR_PATH
 def process_upload_photo(request: Request):
     file = request.files['file']
     tmp_filename = secrets.token_hex() + file.filename
+    file.save(tmp_filename)
     photo_id = len(os.listdir(PHOTOS_DIR_PATH)) + 1
-    # im = Image.open(tmp_filename)
-    # im.save(f'{photo_id}.png')
+    im = Image.open(tmp_filename)
+    im.save(f'{photo_id}.png')
     file.save(os.path.join(PHOTOS_DIR_PATH, tmp_filename))
     os.remove(tmp_filename)
     return jsonify(
