68e8f596a78e088012ddb054a67f2fed14f14dfd
Liza <you@example.com>
2022-10-08 15:17:21 +0300

    add list_achievments

--- src/methods/list_achievements.py
+++ src/methods/list_achievements.py
@@ -1,30 +1,24 @@
+import requests
 from flask import Request, jsonify, abort
 
-from src.config import POSTGRESQL_CONNECTION_PARAMS
+from src.config import POSTGRESQL_CONNECTION_PARAMS, CRYPTO_BASE_URL
 from src.utils.exequte_sql import execute_sql
 
 
 def process_list_achievements(request: Request):
     user_id = request.get_json()['user_id']
-    user_achievements_ids = execute_sql(
-        'SELECT achievement_ids '
-        'FROM users '
-        'WHERE id = %s',
-        (user_id,),
+    user_achievements = execute_sql(
+        'SELECT * '
+        'FROM transactions '
+        'WHERE tx_type = %s AND user_id_to = %s',
+        (3, user_id,),
         POSTGRESQL_CONNECTION_PARAMS,
-    )[0]['achievement_ids']
-    achievements = []
-    for achievement_id in user_achievements_ids:
-        achievement = execute_sql(
-            'SELECT * '
-            'FROM achievements '
-            'WHERE id = %s ',
-            (achievement_id,),
-            POSTGRESQL_CONNECTION_PARAMS,
+    )
+    for i in range(len(user_achievements)):
+        token_id = user_achievements[i]['amount']
+        r = requests.get(
+            f"{CRYPTO_BASE_URL}//v1/nft/{token_id}"
         )
-        achievements.append(achievement)
-    response_dict = {
-        'status': 'ok',
-        'achievements': achievements
-    }
-    return jsonify(response_dict)
+        user_achievements[i]['nft_data'] = r.json()
+
+    return jsonify(user_achievements)
