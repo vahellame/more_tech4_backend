ac2e969ec66c2c75d940d524e82fcbe464e8c189
Vadim R <vahellame@gmail.com>
2022-10-08 12:20:16 +0300

    update app.py

--- src/methods/upload_photo.py
+++ src/methods/upload_photo.py
@@ -11,8 +11,8 @@ def process_upload_photo(request: Request):
     file = request.files['file']
     tmp_filename = secrets.token_hex() + file.filename
     photo_id = len(os.listdir(PHOTOS_DIR_PATH)) + 1
-    im = Image.open(tmp_filename)
-    im.save(f'{photo_id}.png')
+    # im = Image.open(tmp_filename)
+    # im.save(f'{photo_id}.png')
     file.save(os.path.join(PHOTOS_DIR_PATH, tmp_filename))
     os.remove(tmp_filename)
     return jsonify(
