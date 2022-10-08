0e85eb51cea988a985c145f9d348be049975d21f
Liza <you@example.com>
2022-10-08 12:50:48 +0300

    update who and whoami

--- src/methods/who.py
+++ src/methods/who.py
@@ -1,6 +1,7 @@
+import requests
 from flask import jsonify, Request
 
-from src.config import POSTGRESQL_CONNECTION_PARAMS
+from src.config import POSTGRESQL_CONNECTION_PARAMS, CRYPTO_BASE_URL
 from src.utils.exequte_sql import execute_sql
 
 
@@ -14,10 +15,23 @@ def process_who(request: Request):
         POSTGRESQL_CONNECTION_PARAMS,
     )
 
+    public_key = execute_sql(
+        'SELECT public_key '
+        'FROM users '
+        'WHERE id = %s',
+        (user_id,),
+        POSTGRESQL_CONNECTION_PARAMS,
+    )
+
+    balance = requests.get(
+        f'{CRYPTO_BASE_URL}/v1/wallets/{public_key}/balance',
+    )
+
     if len(user_data) != 0:
         response_dict = {
             'status': 'ok',
-            'user': user_data[0]
+            'user': user_data[0],
+            'balance': balance
         }
         return jsonify(response_dict)
     response_dict = {
--- src/methods/whoami.py
+++ src/methods/whoami.py
@@ -1,9 +1,9 @@
 from flask import jsonify, Request
+import requests
 
-from src.config import POSTGRESQL_CONNECTION_PARAMS
+from src.config import POSTGRESQL_CONNECTION_PARAMS, CRYPTO_BASE_URL
 from src.utils.exequte_sql import execute_sql
 from src.utils.get_token_from_request import get_token_from_request
-
 
 def process_whoami(request: Request):
     token = get_token_from_request(request)
@@ -14,15 +14,28 @@ def process_whoami(request: Request):
         (token,),
         POSTGRESQL_CONNECTION_PARAMS,
     )
+    public_key = execute_sql(
+        'SELECT public_key '
+        'FROM users '
+        'WHERE token = %s',
+        (token,),
+        POSTGRESQL_CONNECTION_PARAMS,
+    )
+
+    balance = requests.get(
+        f'{CRYPTO_BASE_URL}/v1/wallets/{public_key}/balance',
+    )
 
     if len(user_data) != 0:
         response_dict = {
             'status': 'ok',
-            'user': user_data[0]
+            'user': user_data[0],
+            'balance': balance
         }
         return jsonify(response_dict)
     response_dict = {
         'status': 'error',
         'error': 'Пользователь не найден',
     }
+
     return jsonify(response_dict)
