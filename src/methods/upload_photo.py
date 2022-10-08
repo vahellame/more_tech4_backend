70543c3a3cab66b0d7a69eea74e1fb6cc86e4759
Vadim R <vahellame@gmail.com>
2022-10-08 11:59:53 +0300

    update upload_photo

--- src/methods/upload_photo.py
+++ src/methods/upload_photo.py
@@ -1,5 +1,7 @@
 import os
+import secrets
 
+from PIL import Image
 from flask import Request, jsonify
 
 from src.config import PHOTOS_DIR_PATH
@@ -7,8 +9,15 @@ from src.config import PHOTOS_DIR_PATH
 
 def process_upload_photo(request: Request):
     file = request.files['file']
-    filename = file.filename
-    file.save(os.path.join(PHOTOS_DIR_PATH, filename))
+    tmp_filename = secrets.token_hex() + file.filename
+    photo_id = len(os.listdir(PHOTOS_DIR_PATH)) + 1
+    im = Image.open(tmp_filename)
+    im.save(f'{photo_id}.png')
+    file.save(os.path.join(PHOTOS_DIR_PATH, tmp_filename))
+    os.remove(tmp_filename)
     return jsonify(
-        {'status': 'ok'}
+        {
+            'status': 'ok',
+            'photo_id': photo_id,
+        }
     )
