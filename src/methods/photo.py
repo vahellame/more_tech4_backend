96c43371d17f0089bf7a43547ff13e1dcf59b469
Vadim R <vahellame@gmail.com>
2022-10-08 10:15:53 +0300

    fix app.py

--- src/methods/photo.py
+++ src/methods/photo.py
@@ -1,7 +1,7 @@
-from flask import send_from_directory
+from flask import send_file
 
 from src.config import FILES_DIR_PATH
 
 
 def process_photo(photo_path):
-    return send_from_directory(directory=FILES_DIR_PATH, path=photo_path)
+    return send_file(FILES_DIR_PATH + '/' + photo_path)
