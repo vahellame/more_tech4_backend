e71219d0099b9a2619fbe7c8aae4c66ea9395def
Liza <you@example.com>
2022-10-09 05:59:40 +0300

    add like unlike products

---
+++ src/methods/list_liked_products.py
@@ -0,0 +1,30 @@
+from flask import jsonify, Request
+
+from src.config import POSTGRESQL_CONNECTION_PARAMS
+from src.utils.exequte_sql import execute_sql
+from src.utils.get_token_from_request import get_token_from_request
+
+
+def process_list_liked_products(request: Request):
+    token = get_token_from_request(request)
+    list_liked_products = execute_sql(
+        'SELECT liked_product_ids '
+        'FROM users '
+        'WHERE token = %s',
+        (token,),
+        POSTGRESQL_CONNECTION_PARAMS,
+    )[0]
+    products = []
+    for product_id in list_liked_products:
+        product = execute_sql(
+            'SELECT * '
+            'FROM products '
+            'WHERE id = %s',
+            (product_id,),
+            POSTGRESQL_CONNECTION_PARAMS,
+        )
+        products.append(product)
+    return jsonify({
+        'products': products,
+        'status': 'ok'
+    })
---
+++ src/methods/unlike_product.py
@@ -0,0 +1,28 @@
+from flask import jsonify, Request
+
+from src.config import POSTGRESQL_CONNECTION_PARAMS
+from src.utils.exequte_sql import execute_sql
+from src.utils.get_token_from_request import get_token_from_request
+
+
+def process_unlike_product(request: Request):
+    token = get_token_from_request(request)
+    product_id = request.get_json()['product_id']
+    list_liked_products = execute_sql(
+        'SELECT liked_product_ids '
+        'FROM users '
+        'WHERE token = %s',
+        (token,),
+        POSTGRESQL_CONNECTION_PARAMS,
+    )
+    new_list_liked_products = list(list_liked_products).remove(product_id)
+    execute_sql(
+        'UPDATE users '
+        'SET liked_product_ids = %s '
+        'WHERE token = %s',
+        (new_list_liked_products, token,),
+        POSTGRESQL_CONNECTION_PARAMS,
+        )
+    return jsonify({
+        'status': 'ok'
+    })
